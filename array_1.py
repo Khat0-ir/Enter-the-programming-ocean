f4209b91238b6b1cb9a5b6ecfb3b3429cdd70e76
saeidfiy <saeidfiy77@gmail.com>
2024-09-18 10:26:43 +0330

    add files

---
+++ array_1.py
@@ -0,0 +1,13 @@
+a = ['khat0','kaht1','kaht2','khat3',4,5,6,7]
+
+a.append('saeidfiy')
+
+a.pop(2)
+
+a.remove('khat3')
+
+print(a)
+
+print(len(a))
+
+print(a[0:3])
---
+++ string_1.py
@@ -0,0 +1,8 @@
+a = "khat0.ir"
+
+# print(len(a))
+
+# print(a[2:5])
+
+for str in a:
+    print(str)
